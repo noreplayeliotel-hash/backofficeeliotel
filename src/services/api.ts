d70f629508ef9ac3880ff0ffb0b5219bf3d2ca05
noreplayeliotel-hash <noreplayeliotel@gmail.com>
2026-09-14 17:39:21 +0100

    Add files via upload

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://api.eliotel.com/api',
+    baseURL: import.meta.env.VITE_API_URL || 'http://172.16.103.13:3002/api',
     headers: {
         'Content-Type': 'application/json',
     },
