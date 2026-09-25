ded7395ee0d8064dc0e1b1bb5a97d0402f821a5b
noreplayeliotel-hash <noreplayeliotel@gmail.com>
2026-09-25 09:47:29 +0100

    Delete src directory

--- src/components/bookings/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as BookingFilters } from './BookingFilters';
-export { default as BookingCard } from './BookingCard';
-export { default as BookingTable } from './BookingTable';
-export { default as CreateBookingModal } from './CreateBookingModal';
-export { default as CreateUserModal } from './CreateUserModal';
